395985fb5dc88ef624ba1b1715bcaa5288cf8be2
Markus Schmidt <schmidt@boomshop.net>
2021-03-23 01:15:49 +0100

    Minor changes to templates

--- htdocs/js/templates/OcaUint8Sensor.js
+++ htdocs/js/templates/OcaUint8Sensor.js
@@ -3,17 +3,15 @@ import { registerTemplateComponent } from '../template_components.js';
 
 const template = `
 <awml-option sync type=bind src='/Role' name=Role readonly></awml-option>
-<div>
-  {{ this.Role }}<br>
-  <aux-levelmeter
-    style='height: 200px;' falling=100 show_value value.format='sprintf:%d'
-    foreground='white' background='grey'
-    >
-    <awml-option type=bind src='/Reading' name=value readonly></awml-option>
-    <awml-option type=bind src='/Reading/Min' name=min readonly></awml-option>
-    <awml-option type=bind src='/Reading/Max' name=max readonly></awml-option>
-  </aux-levelmeter>
-</div>
+<aux-levelmeter
+  label="{{ this.Role }}"
+  style='height: 200px;' falling=100 show_value value.format='sprintf:%d'
+  foreground='white' background='grey'
+  >
+  <awml-option type=bind src='/Reading' name=value readonly></awml-option>
+  <awml-option type=bind src='/Reading/Min' name=min readonly></awml-option>
+  <awml-option type=bind src='/Reading/Max' name=max readonly></awml-option>
+</aux-levelmeter>
 `;
 
 class OcaUint8SensorTemplate extends TemplateComponent.fromString(template) {
