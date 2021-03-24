0671af8f264cfbc7797c74011b33ae3fee8b576f
Markus Schmidt <schmidt@boomshop.net>
2021-03-24 01:45:20 +0100

    OcaUint8Sensor: switch from DOM bindings to %bind, add first attempt of gradient generation

--- htdocs/js/templates/OcaUint8Sensor.js
+++ htdocs/js/templates/OcaUint8Sensor.js
@@ -1,20 +1,56 @@
 import { TemplateComponent } from '../../AWML/src/index.pure.js';
 import { registerTemplateComponent } from '../template_components.js';
 
+const colorMax = '#ff6600';
+const colorMid = '#8b06a5';
+const colorMin = '#280062';
+
 const template = `
 <awml-option sync type=bind src='/Role' name=Role readonly></awml-option>
 <aux-levelmeter
-  label="{{ this.Role }}"
-  style='height: 200px;' falling=100 show_value value.format='sprintf:%d'
-  foreground='white' background='grey'
+  style='height: 200px;'
+  falling=100
+  show_value=true
+  value.format='sprintf:%.1f'
+  foreground='black'
   >
-  <awml-option type=bind src='/Reading' name=value readonly></awml-option>
-  <awml-option type=bind src='/Reading/Min' name=min readonly></awml-option>
-  <awml-option type=bind src='/Reading/Max' name=max readonly></awml-option>
 </aux-levelmeter>
 `;
 
 class OcaUint8SensorTemplate extends TemplateComponent.fromString(template) {
+  constructor() {
+    super();
+    this.meterBindings = [
+      {
+        src: '/Role',
+        name: 'label',
+      },
+      {
+        src: '/Reading',
+        name: 'value',
+      },
+      {
+        src: '/Reading/Min',
+        name: 'min',
+      },
+      {
+        src: '/Reading/Max',
+        name: 'max',
+      },
+      {
+        src: '/Reading/Min,/Reading/Max',
+        name: 'gradient',
+        transformReceive: (arr) => {
+          const [min, max] = arr;
+          let grad = {};
+          grad[String(min)] = colorMin;
+          grad[String(min + (max - min) / 2)] = colorMid;
+          grad[max] = colorMax;
+          return grad;
+        },
+      },
+    ];
+  }
   static match(o) {
     return o.ClassName.endsWith('Sensor');
   }
