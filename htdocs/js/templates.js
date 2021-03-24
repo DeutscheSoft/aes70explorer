8f487d6d51974becedb8b90963e30bb568266145
Markus Schmidt <schmidt@boomshop.net>
2021-03-24 02:27:06 +0100

    OcaBooleanActuator

--- htdocs/js/templates.js
+++ htdocs/js/templates.js
@@ -1,2 +1,3 @@
 import './templates/OcaGain.js';
+import './templates/OcaBooleanActuator.js';
 import './templates/OcaUint8Sensor.js';
---
+++ htdocs/js/templates/OcaBooleanActuator.js
@@ -0,0 +1,30 @@
+import { TemplateComponent } from '../../AWML/src/index.pure.js';
+import { registerTemplateComponent } from '../template_components.js';
+
+const template = `
+<aux-toggle %bind={{ this.faderBindings }}></aux-toggle>
+`;
+
+class OcaBooleanActuatorTemplate extends TemplateComponent.fromString(template) {
+  constructor() {
+    super();
+    this.faderBindings = [
+      {
+        src: '/Role',
+        name: 'label',
+      },
+      {
+        src: '/Setting',
+        name: 'state',
+      },
+    ];
+  }
+  static match(o) {
+    return [
+      'OcaBooleanActuator',
+      'OcaMute',
+    ].indexOf(o.ClassName) >= 0;
+  }
+}
+
+registerTemplateComponent(OcaBooleanActuatorTemplate);
