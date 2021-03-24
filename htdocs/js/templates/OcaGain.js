45eaa5ea4b21d2ccab3b99dc0ed0b84ca607cbcc
Markus Schmidt <schmidt@boomshop.net>
2021-03-24 01:44:25 +0100

    OcaGain: switch from DOM bindings to %bind

--- htdocs/js/templates/OcaGain.js
+++ htdocs/js/templates/OcaGain.js
@@ -3,16 +3,34 @@ import { registerTemplateComponent } from '../template_components.js';
 
 const template = `
 <aux-fader
-  label='{{ this.Role }}'
+  %bind={{ this.faderBindings }}
   show_value="true"
   value.format="sprintf:%.2f">
-  <awml-option type=bind src='/Gain' name=value></awml-option>
-  <awml-option type=bind src='/Gain/Min' name=min readonly></awml-option>
-  <awml-option type=bind src='/Gain/Max' name=max readonly></awml-option>
 </aux-fader>
 `;
 
 class OcaGainTemplate extends TemplateComponent.fromString(template) {
+  constructor() {
+    super();
+    this.faderBindings = [
+      {
+        src: '/Role',
+        name: 'label',
+      },
+      {
+        src: '/Gain',
+        name: 'value',
+      },
+      {
+        src: '/Gain/Min',
+        name: 'min',
+      },
+      {
+        src: '/Gain/Max',
+        name: 'max',
+      },
+    ];
+  }
   static match(o) {
     return o.ClassName === 'OcaGain';
   }
