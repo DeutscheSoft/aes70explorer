04ad23510050bc019a8c9931ea922b4caf505b8c
Markus Schmidt <schmidt@boomshop.net>
2021-03-25 22:49:26 +0100

    aes70-object: dedicated add and remove buttons

--- htdocs/js/widgets/aes70_object.js
+++ htdocs/js/widgets/aes70_object.js
@@ -2,17 +2,33 @@ import { collectPrefix, TemplateComponent } from '../../AWML/src/index.pure.js';
 import { addToCanvas } from '../layout.js';
 
 const template = `
-<div class=head (dblclick)={{ this.onHeadDblClick }}>
+<div class={{ this.classes }}>
   <aux-icon %bind={{this.iconBindings}} class=icon></aux-icon>
   <aux-label %bind={{this.labelBindings}} class=label></aux-label>
   <aux-label %bind={{this.classBindings}} class=class></aux-label>
+  <aux-button
+    class=add
+    icon=puzzle
+    (click)={{ this.onAddClicked }}
+    %if={{ !this._controlNode }}
+  ></aux-button>
+  <aux-confirmbutton
+    class=remove
+    icon=trash
+    icon_confirm=confirm
+    (confirmed)={{ this.onRemoveClicked }}
+    %if={{ !!this._controlNode }}
+  ></aux-confirmbutton>
 </div>
 `;
 
 class AES70Object extends TemplateComponent.fromString(template) {
   constructor() {
     super();
-    this._templateNode = null;
+    this._controlNode = null;
+    this._classesSet = new Set();
+    this._classesSet.add('head');
+    this.classes = 'head';
     this.iconBindings = [{
       src: '',
       name: 'icon',
@@ -27,13 +43,22 @@ class AES70Object extends TemplateComponent.fromString(template) {
       name: 'label',
       transformReceive: v=>v.ClassName,
     }];
-    this.onHeadDblClick = (e) => {
-      if (this._templateNode) return; 
-      const node = this._createTemplateNode();
-      this._templateNode = addToCanvas(node);
+    this.onAddClicked = (e) => {
+      if (this._controlNode) return; 
+      const node = this._createControlNode();
+      this._controlNode = addToCanvas(node);
+      this._classesSet.add('hascontrol');
+      this.setClasses();
+    }
+    this.onRemoveClicked = (e) => {
+      if (!this._controlNode) return;
+      this._controlNode.remove(); 
+      this._controlNode = null;
+      this._classesSet.delete('hascontrol');
+      this.setClasses();
     }
   }
-  _createTemplateNode() {
+  _createControlNode() {
     const node = document.createElement('aes70-object-control');
 
     node.setAttribute('prefix', collectPrefix(this));
@@ -50,6 +75,10 @@ class AES70Object extends TemplateComponent.fromString(template) {
   connectedCallback() {
     super.connectedCallback();
     this.style.display = null;
+  }
+  
+  setClasses() {
+    this.classes = Array.from(this._classesSet).join(' ');
   }
 }
 
