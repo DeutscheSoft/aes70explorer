3041bbac4162994fa34145fd76c66311dbdea005
Markus Schmidt <schmidt@boomshop.net>
2021-03-26 13:55:19 +0100

    Browser: fix knob presets

--- htdocs/js/controls/OcaUint8Actuator.js
+++ htdocs/js/controls/OcaUint8Actuator.js
@@ -1,12 +1,14 @@
 import { TemplateComponent, DynamicValue } from '../../AWML/src/index.pure.js';
 import { registerTemplateControl } from '../template_components.js';
 
+// knob.presets={{ "json:" + JSON.stringify(this.knobPresets) }}
+
 const template = `
 <aux-valueknob #knob
-  knob.presets={{ this.knobPresets }}
   value.format='sprintf:%.2f'
   knob.show_hand=false
   %bind={{ this.knobBindings }}
+  knob.preset=medium
   >
 </aux-valueknob>
 <aux-button icon=edit (click)={{ this.editClicked }}></aux-button>
@@ -15,7 +17,7 @@ const template = `
 const knobPresets = {
   tiny: {
     margin: 0,
-    thickness: 4,
+    thickness: 5,
     hand: { width: 1, length: 6, margin: 8 },
     dots_defaults: { length: 4, margin: 0.5, width: 1 },
     markers_defaults: { thickness: 2, margin: 0 },
@@ -23,7 +25,7 @@ const knobPresets = {
   },
   small: {
     margin: 8,
-    thickness: 4.5,
+    thickness: 4,
     hand: { width: 1, length: 8, margin: 17 },
     dots_defaults: { length: 4.5, margin: 8.5, width: 1 },
     markers_defaults: { thickness: 2, margin: 8 },
@@ -32,7 +34,7 @@ const knobPresets = {
   },
   medium: {
     margin: 13,
-    thickness: 6,
+    thickness: 3,
     hand: { width: 1, length: 10, margin: 25 },
     dots_defaults: { length: 6, margin: 13.5, width: 1 },
     markers_defaults: { thickness: 2, margin: 11 },
@@ -40,18 +42,20 @@ const knobPresets = {
   },
   large: { 
     margin: 13,
-    thickness: 6,
-    hand: { width: 1.5, length: 12, margin: 26 } },
+    thickness: 2.2,
+    hand: { width: 1.5, length: 12, margin: 26 },
     dots_defaults: { length: 6, margin: 13.5, width: 1 },
     markers_defaults: { thickness: 2, margin: 11 },
     show_labels: true,
+  },
   huge: {
     margin: 13,
-    thickness: 6,
-    hand: { width: 2, length: 12, margin: 28 } },
+    thickness: 1.8,
+    hand: { width: 2, length: 12, margin: 28 },
     dots_defaults: { length: 6, margin: 13.5, width: 1 },
     markers_defaults: { thickness: 2, margin: 11 },
     show_labels: true,
+  },
 }
     
 class OcaUint8ActuatorControl extends TemplateComponent.fromString(template) {
@@ -82,7 +86,6 @@ class OcaUint8ActuatorControl extends TemplateComponent.fromString(template) {
       {
         backendValue: this.knobPresets,
         name: 'knob.presets',
-        format: 'js',
       }
     ];
     this.editClicked = (e) => {
