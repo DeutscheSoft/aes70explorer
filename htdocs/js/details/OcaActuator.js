0d22bbe746d148eda2e9ddfa04e30bcde02455c8
Markus Schmidt <schmidt@boomshop.net>
2021-04-01 00:27:39 +0200

    Browser: details have label and enabled toggle, nicer layout for details

--- htdocs/js/details/OcaActuator.js
+++ htdocs/js/details/OcaActuator.js
@@ -2,32 +2,36 @@ import { TemplateComponent, DynamicValue } from '../../AWML/src/index.pure.js';
 import { registerTemplateDetails } from '../template_components.js';
 
 const template = `
+<span class="label">Label</span>
+<aux-value preset=string %bind={{ this.LabelBind }}></aux-value>
+
+<span class="label">Enabled</span>
+<aux-toggle icon=power %bind={{ this.EnabledBind }}></aux-toggle>
+
 <span class="label">ClassVersion</span>
 <aux-label %bind={{ this.ClassVersionBind }}></aux-label>
 
 <span class="label">Lockable</span>
 <aux-label %bind={{ this.LockableBind }}></aux-label>
 
-<span class="label">Enabled</span>
-<aux-label %bind={{ this.EnabledBind }}></aux-label>
-
-<span class="label">Setting</span>
-<aux-label %bind={{ this.SettingBind }}></aux-label>
-
 <span class="label">Min</span>
 <aux-label %bind={{ this.MinBind }}></aux-label>
 
 <span class="label">Max</span>
 <aux-label %bind={{ this.MaxBind }}></aux-label>
+
+<span class="label">Setting</span>
+<aux-label %bind={{ this.SettingBind }}></aux-label>
 `;
 
 class OcaActuatorDetails extends TemplateComponent.fromString(template) {
   constructor() {
     super();
     
+    this.LabelBind = [{ src: '/Label', name: 'value' }];
     this.ClassVersionBind = [{ src: '/ClassVersion', name: 'label' }];
     this.LockableBind = [{ src: '/Lockable', name: 'label' }];
-    this.EnabledBind = [{ src: '/Enabled', name: 'label' }];
+    this.EnabledBind = [{ src: '/Enabled', name: 'state' }];
     this.SettingBind = [{ src: '/Setting', name: 'label' }];
     this.MinBind = [{ src: '/Setting/Min', name: 'label' }];
     this.MaxBind = [{ src: '/Setting/Max', name: 'label' }];
