1ba0bcfbd8eb80daafe2d32c198a9a179a58bcb8
Markus Schmidt <schmidt@boomshop.net>
2021-04-18 16:09:32 +0200

    Browser: add new control after selected control if any

--- htdocs/js/layout.js
+++ htdocs/js/layout.js
@@ -1,6 +1,15 @@
-export function addToCanvas(node) {
-  document.querySelector('#canvas').appendChild(node);
+import { findControl } from './template_components.js';
+import { getBackendValue } from './../AWML/src/index.pure.js';
 
+export function addToCanvas(node) {
+  const path = getBackendValue('local:selected')._value;
+  const canvas = document.querySelector('#canvas');
+  let selected, next;
+  if (path && (selected = findControl(path)) && (next = selected.nextSibling)) {
+    canvas.insertBefore(node, next);
+  } else {
+    canvas.appendChild(node);
+  }
   // This might in the future return a layout-node which places its child
   // in a specific position on the canvas
   return node;
