2e075f28e892be2af633f181df4291a298cfc308
Markus Schmidt <schmidt@boomshop.net>
2021-05-20 00:32:35 +0200

    OcaBooleanSensor: control

--- htdocs/js/controls/OcaBooleanSensor.js
+++ htdocs/js/controls/OcaBooleanSensor.js
@@ -2,12 +2,20 @@ import { TemplateComponent } from '../../AWML/src/index.pure.js';
 import { matchClass, registerTemplateControl } from '../template_components.js';
 
 const template = `
+<aux-label %bind={{ this.labelBindings }}></aux-label>
 <aux-state %bind={{ this.stateBindings }}></aux-state>
 `;
 
 class OcaBooleanSensorControl extends TemplateComponent.fromString(template) {
   constructor() {
     super();
+    this.labelBindings = [
+      {
+        src: '/Role',
+        name: 'label',
+        readonly: true,
+      }
+    ];
     this.stateBindings = [
       {
         src: '/Reading',
