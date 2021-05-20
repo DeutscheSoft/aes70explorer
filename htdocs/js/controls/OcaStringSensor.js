b9a724dab87812a8944c092cb45d2e1ae702993b
Markus Schmidt <schmidt@boomshop.net>
2021-05-20 21:54:08 +0200

    OcaStringSensor

--- htdocs/js/controls/OcaStringSensor.js
+++ htdocs/js/controls/OcaStringSensor.js
@@ -4,12 +4,20 @@ import { matchClass, registerTemplateControl } from '../template_components.js';
 
 const template = `
 <aux-label %bind={{ this.labelBindings }}></aux-label>
+<aux-label %bind={{ this.sensorBindings }}></aux-label>
 `;
 
 class OcaStringSensorControl extends TemplateComponent.fromString(template) {
   constructor() {
     super();
     this.labelBindings = [
+      {
+        src: '/Role',
+        name: 'label',
+        readonly: true,
+      }
+    ];
+    this.sensorBindings = [
       {
         src: '/String',
         name: 'label',
