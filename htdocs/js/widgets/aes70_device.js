adc0abe6dfeb3e04709c95229c9307782022aa6b
Markus Schmidt <schmidt@boomshop.net>
2021-05-26 21:48:23 +0200

    Navigation: add add and remove button to device header

--- htdocs/js/widgets/aes70_device.js
+++ htdocs/js/widgets/aes70_device.js
@@ -1,4 +1,5 @@
 import { TemplateComponent, getBackendValue, collectPrefix, fromSubscription } from '../../AWML/src/index.pure.js';
+import { addControlToCanvas, removeControlFromCanvas } from '../layout.js';
 
 const Selected = getBackendValue('local:selected');
 
@@ -11,6 +12,21 @@ const templateComponent = TemplateComponent.create({
   <aux-icon class='iport' icon='port'></aux-icon>
   <aux-label class='host' label='{{ this.info.host }}'></aux-label>
   <aux-label class='port' label='{{ this.info.port }}'></aux-label>
+  <aux-button
+    class=add
+    icon=puzzle
+    (click)={{ this.onAddClicked }}
+    %if={{ !this._hasControl }}
+  ></aux-button>
+  <aux-confirmbutton timeout=3000
+    class=remove
+    icon=trash
+    icon_confirm=confirm
+    (confirmed)={{ this.onRemoveConfirmed }}
+    (click)={{ this.onRemoveClicked }}
+    (canceled)={{ this.onRemoveCanceled }}
+    %if={{ !!this._hasControl }}
+  ></aux-confirmbutton>
 </div>
 <aes70-block-children %if={{ this.open }} prefix={{ this.info.name + ':' }}></aes70-block-children>
 `,
@@ -54,6 +70,22 @@ class AES70Device extends templateComponent {
       this.open = !this.open;
       ev.stopPropagation();
       this.select();
+    }
+    this.onAddClicked = (e) => {
+      //this._addControl();
+    }
+    this.onRemoveConfirmed = (e) => {
+      //this._removeControl();
+    }
+    this.onRemoveClicked = (e) => {
+      //const node = getRegisteredControl(collectPrefix(this));
+      //if (!node) return;
+      //node.classList.add('scaffold');
+    }
+    this.onRemoveCanceled = (e) => {
+      //const node = getRegisteredControl(collectPrefix(this));
+      //if (!node) return;
+      //node.classList.remove('scaffold');
     }
   }
 }
