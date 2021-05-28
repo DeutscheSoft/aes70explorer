9d3f8d560a337898bc3feaaff0b587a431d6f25e
Markus Schmidt <schmidt@boomshop.net>
2021-05-28 19:38:29 +0200

    Generic device: content of control and styles

--- htdocs/js/device_controls/Generic.js
+++ htdocs/js/device_controls/Generic.js
@@ -4,13 +4,51 @@ import { registerDeviceControlTemplate } from '../device_controls.js';
 import { objectDetailPrefix } from '../object_details.js';
 
 const template = `
-Generic device:
-<${objectDetailPrefix}-devicemanager prefix='/DeviceManager'></${objectDetailPrefix}-devicemanager>
+<aux-label %bind={{ this.ManufacturerBind }} class=manufacturer></aux-label>
+<aux-label %bind={{ this.LabelBind }} class=label></aux-label>
+<aux-label %bind={{ this.VersionBind }} class=version></aux-label>
+<aux-label %bind={{ this.SerialBind }} class=serial></aux-label>
 `;
 
 class GenericDeviceControl extends TemplateComponent.fromString(template) {
   constructor() {
     super();
+    
+    this.LabelBind = [
+      {
+        src: ['/DeviceManager/DeviceName', '/DeviceManager/ModelDescription'],
+        name: 'label',
+        readonly: true,
+        transformReceive: arr => {
+          const [name, desc] = arr;
+          return name ? name : desc.Model;
+        },
+        debug: true,
+      }
+    ];
+    this.ManufacturerBind = [
+      {
+        src: '/DeviceManager/ModelDescription',
+        name: 'label',
+        readonly: true,
+        transformReceive: v => v.Manufacturer,
+      }
+    ];
+    this.SerialBind = [
+      {
+        src: '/DeviceManager/SerialNumber',
+        name: 'label',
+        readonly: true,
+      }
+    ];
+    this.VersionBind = [
+      {
+        src: '/DeviceManager/ModelDescription',
+        name: 'label',
+        readonly: true,
+        transformReceive: v => v.Version,
+      }
+    ];
   }
 
   static match(o) {
