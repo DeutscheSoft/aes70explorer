18c8a2d0863b1e57ec42d4498798b0493ba8246d
Arne Goedeke <arne@deuso.de>
2021-05-28 12:45:22 +0200

    DeviceControls: Remove extra - in tagNames

--- htdocs/js/device_controls.js
+++ htdocs/js/device_controls.js
@@ -1,6 +1,6 @@
 import { TemplateRegistry } from './utils/template_registry.js';
 
-export const deviceControlTemplateRegistry = new TemplateRegistry('aes70-device-');
+export const deviceControlTemplateRegistry = new TemplateRegistry('aes70-device-control');
 
 export async function createDeviceControlComponent(o) {
   const tagName = await deviceControlTemplateRegistry.find(o);
