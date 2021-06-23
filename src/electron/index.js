85908aa6d7bd20f561b40b1bae98837a0b0be646
Arne Goedeke <arne@deuso.de>
2021-06-23 17:58:09 +0200

    Added simple Electron app

---
+++ src/electron/index.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow } = require('electron');
+const pServer = import('../nodejs/lib/backend.js');
+const { dirname, join } = require('path');
+
+const defaultHtdocs = join(dirname(__filename), "../../htdocs");
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    show: false,
+    title: 'AES70 Explorer',
+  })
+
+  win.setMenuBarVisibility(false);
+  win.removeMenu();
+  win.loadURL('http://localhost:8080')
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+  win.on('leave-full-screen', () => {
+    win.removeMenu();
+    win.setMenuBarVisibility(false);
+  });
+}
+
+let backend = null;
+
+async function startUp() {
+  const { Backend } = await pServer;
+
+  await app.whenReady()
+
+  backend = new Backend({
+    http: {
+      port: 8080,
+      htdocs: defaultHtdocs,
+    }
+  });
+  await backend.start();
+
+  createWindow()
+}
+
+startUp().catch(
+  (err) => {
+    console.error(err);
+    process.exit(1);
+  }
+);
+
+
