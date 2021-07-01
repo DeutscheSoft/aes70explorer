3cebe512aded75321dca2d6e8cca65cdf3602fe7
Arne Goedeke <arne@deuso.de>
2021-07-01 14:38:23 +0200

    NodeJS backend: Replace hapi by standard http module

--- src/nodejs-lib/src/http.js
+++ src/nodejs-lib/src/http.js
@@ -1,8 +1,12 @@
-import { Server } from '@hapi/hapi';
-import HapiPluginInert from '@hapi/inert';
+import { createServer } from 'http';
+import { lookup } from 'mime-types';
 import { connectTCPTunnel } from './websocket_tcp_tunnel.js';
-import path from 'path';
+import { extname, join, dirname } from 'path';
 import WS from 'ws';
+import fs from 'fs';
+const { readFile, stat } = fs.promises;
+
+const controlPrefix = "/_control/";
 
 /**
  * Start the web server.
@@ -16,28 +20,105 @@ import WS from 'ws';
  *      with the name `<NAME>`.
  */
 export default async function start(config, destinations) {
-  const server = new Server({ port: config.port });
-
-  await server.register({ plugin: HapiPluginInert });
-
-  const prefix = "/_control/";
-
   // FIXME
   const token = "random_secret";
+  const htdocs = config.htdocs;
+
+  const server = createServer(async (req, res) => {
+    const url = new URL(req.url, 'http://localhost/');
+    let path = decodeURI(url.pathname);
+
+    if (path === '') {
+      path = '/';
+    }
+
+    if (path.endsWith('/')) {
+      path += 'index.html';
+    }
+
+    switch (path) {
+      case '/_control_token':
+        return txt(websocketToken);
+    }
+
+    const headers = {
+      'Cache-Control': 'no-store',
+    };
+
+    const respond = (data, code, headers) => {
+      if (!code) code = 200;
+      res.writeHead(code, headers);
+      res.end(data, 'binary');
+    };
+
+    const txt = (data, code) => {
+      respond(
+        data,
+        code,
+        Object.assign(headers, { 'Content-Type': 'text/plain' })
+      );
+    };
+
+    const json = (data, code) => {
+      respond(
+        JSON.stringify(data),
+        code,
+        Object.assign(headers, { 'Content-Type': 'application/json' })
+      );
+    };
+
+    switch (path) {
+    case '/_api/token':
+        return txt(token);
+    case '/_api/destinations':
+        return json(Array.from(destinations.values()));
+    }
+
+    const fname = join(htdocs, path);
+
+    if (!fname.startsWith(htdocs)) {
+      return txt('', 404);
+    }
+
+    try {
+      const data = await readFile(fname, { encoding: 'binary' });
+      return respond(
+        data,
+        200,
+        Object.assign(headers, { 'Content-Type': lookup(extname(fname)) })
+      );
+    } catch (err) {}
+
+    try {
+      const stats = await stat(fname);
+
+      if (stats.isDirectory()) {
+        return respond(
+          '',
+          302,
+          Object.assign(headers, { Location: path + '/' })
+        );
+      }
+    } catch (err) {}
+
+    console.log('HTTP 404 - %o not found in %o.', path, htdocs);
+
+    return respond('', 404);
+  });
 
   const wss = new WS.Server({
     clientTracking: false,
     noServer: true,
   });
 
-  server.listener.on('upgrade', async (request, socket, head) => {
+  server.on('upgrade', async (request, socket, head) => {
     try {
       let destination;
       const url = request.url;
 
-      if (url.startsWith(prefix))
+      if (url.startsWith(controlPrefix))
       {
-        const destinationName = url.substr(prefix.length);
+        const destinationName = url.substr(controlPrefix.length);
 
         destination = destinations.get(destinationName);
       }
@@ -63,31 +144,11 @@ export default async function start(config, destinations) {
     }
   });
 
-  server.route({
-    method: 'GET',
-    path: '/_api/destinations',
-    handler: (request, h) => {
-      return Array.from(destinations.values());
-    },
-  });
+  console.log('serving files from %o %o', process.cwd, config.htdocs);
 
-  server.route({
-    method: 'GET',
-    path: '/_api/token',
-    handler: (request, h) => {
-      return token;
-    },
+  return new Promise((resolve) => {
+    server.listen(config.port, () => {
+      resolve();
+    });
   });
-
-  server.route({
-      method: 'GET',
-      path: '/{path*}',
-      handler: {
-          directory: {
-              path: config.htdocs,
-          }
-      }
-  });
-
-  await server.start();
 }
