03eca9318b1a37327c963a891f4732c6bc423927
Markus Schmidt <markus@deuso.de>
2021-07-02 00:01:11 +0200

    Browser: add device handling and UX

--- htdocs/js/widgets/aes70_add_device.js
+++ htdocs/js/widgets/aes70_add_device.js
@@ -6,8 +6,8 @@ const template = `
 <div class="container {{ this.open ? 'open':'close' }}">
   <aux-label label="URL" class=lurl></aux-label>
   <aux-label label="Port" class=lport></aux-label>
-  <aux-value editmode=immediate class=url preset=string #url placeholder="URL or IP Address"></aux-value>
-  <aux-value editmode=immediate class=port preset=string #port placeholder="Port"></aux-value>
+  <aux-value editmode=immediate class=url preset=string #url placeholder="URL or IP Address" (keyup)={{ this.inputKeyup }}></aux-value>
+  <aux-value editmode=immediate class=port preset=string #port placeholder="Port" (keyup)={{ this.inputKeyup }}></aux-value>
   <aux-button class=ok icon="ok" label="Add Device" (click)={{ this.onOKClick }}>
   </aux-button>
   <aux-button class=cancel icon="cancel" label="Cancel" (click)={{ this.onCancelClick }}>
@@ -30,18 +30,30 @@ class AES70AddDevice extends TemplateComponent.fromString(template) {
     
     this.onAddClick = (ev) => {
       this.open = !this.open;
+      if (this.open)
+        this.url.auxWidget._input.focus();
     }
     
     this.onOKClick = (ev) => {
       this.open = false;
-      const url = this.url.auxWidget.get('value');
-      const port = this.port.auxWidget.get('value');
-      // DO SOMETHING USEFUL
-      console.log(url, port);
+      this.addDevice();
     }
     
     this.onCancelClick = (ev) => {
       this.open = false;
+    }
+    
+    this.inputKeyup = (ev) => {
+      if(ev.key === "Enter") {
+        this.addDevice();
+        this.url.auxWidget._input.focus();
+      }
+    }
+    this.addDevice = () => {
+      const url = this.url.auxWidget.get('value');
+      const port = this.port.auxWidget.get('value');
+      // DO SOMETHING USEFUL
+      console.log(url, port);
     }
   }
   
