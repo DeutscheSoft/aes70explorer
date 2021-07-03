706fdebcf5bc065a358673d3d540152d6fe65a12
Arne Goedeke <arne@deuso.de>
2021-07-03 13:50:25 +0200

    NodeJS Standalone: Use relative imports again
    Using npm dependencies using relative file urls does not work correctly
    with depencies.

--- src/nodejs/server.js
+++ src/nodejs/server.js
@@ -1,7 +1,7 @@
 import yargs from 'yargs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import { Backend } from 'aes70-explorer-nodejs-lib';
+import { Backend } from '../nodejs-lib/src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const defaultHtdocs = join(dirname(__filename), "../../htdocs");
