8c0a17f55e12c4a77ec8aca08f7c3efb7b539b67
Arne Goedeke <arne@deuso.de>
2021-10-14 15:13:50 +0200

    Hide add device widget based on capabilities

--- htdocs/js/widgets/aes70_navigation.js
+++ htdocs/js/widgets/aes70_navigation.js
@@ -5,15 +5,23 @@ import { forEachAsync } from '../utils.js';
 const template = `
 <div class=list>
   <div #scroller class=scroller></div>
-  <div class=nodevice %if={{ !this.hasDevices }}>Searching for<br>Devices...<br><br>Add Devices<br>at the Bottom</div>
+  <div class=nodevice %if={{ !this.hasDevices }}>Searching for<br>Devices...<br><br><span %if={{ this.manualDevices }}>Add Devices<br>at the Bottom</span></div>
 </div>
+<aes70-add-device %if={{ this.manualDevices }}></aes70-add-device>
 `;
 
 class AES70Navigation extends TemplateComponent.fromString(template) {
+  static getHostBindings() {
+    return [
+      {name: 'manualDevices', src: 'capabilities:manual_devices', readonly: true, sync: true},
+    ];
+  }
+
   constructor() {
     super();
     
     this.hasDevices = false;
+    this.manualDevices = false;
     
     forEachAsync(
       Devices,
