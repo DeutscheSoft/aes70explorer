77850165e9c15e1bafe59b8165302e0d23ce1c70
Arne Goedeke <arne@deuso.de>
2022-11-11 15:50:16 +0100

    MDNSDiscovery: Handle duplicate mdns announcements gracefully
    If the same service announcement is emitted from the dnssd library,
    simply do nothing instead of crashing.
    
    Fixes #102

--- src/nodejs/lib/discovery.js
+++ src/nodejs/lib/discovery.js
@@ -20,10 +20,47 @@ function createDestination(service) {
   };
 }
 
+class CountingMap {
+  constructor() {
+    this._counters = new Map();
+    this._sub = new Map();
+  }
+
+  set(key, factory) {
+    const { _counters, _sub } = this;
+
+    if (_counters.has(key)) {
+      _counters.set(key, _counters.get(key) + 1);
+    } else {
+      _counters.set(key, 1);
+      _sub.set(key, factory());
+    }
+  }
+
+  delete(key) {
+    const { _counters, _sub } = this;
+
+    if (!_counters.has(key))
+      return;
+
+    const count = _counters.get(key);
+
+    if (count >= 1) {
+      const sub = _sub.get(key);
+
+      _counters.delete(key);
+      sub();
+    } else {
+      _counters.set(key, count - 1);
+    }
+  }
+
+}
+
 export default function start(addDestination) {
   const tcpBrowser = new Browser(ServiceType.tcp('oca'));
   const udpBrowser = new Browser(ServiceType.udp('oca'));
-  const registered = new Map();
+  const registered = new CountingMap();
 
   const onError = (err) => {
     console.error('Error on dnssd browser: %o', err);
@@ -32,7 +69,7 @@ export default function start(addDestination) {
   const onServiceUp = (service) => {
     if (service.type.protocol !== 'tcp') return;
     console.log('Service %o', service);
-    registered.set(service.fullname, addDestination(createDestination(service)));
+    registered.set(service.fullname, () => addDestination(createDestination(service)));
   };
 
   const onServiceDown = (service) => {
@@ -40,12 +77,7 @@ export default function start(addDestination) {
         service.type.protocol !== 'udp') return;
     const fullname = service.fullname;
 
-    const sub = registered.get(fullname);
-
-    if (sub) {
-      registered.delete(fullname);
-      sub();
-    }
+    registered.delete(fullname);
   };
 
   tcpBrowser.on('error', onError);
